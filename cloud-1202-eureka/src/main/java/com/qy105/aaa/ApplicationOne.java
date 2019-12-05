7915a5145fb1beed551c00c977da81d6e394eec9
cat <1607925416@qq.com>
2019-12-05 20:53:54 +0800

    first commit

---
+++ cloud-1202-eureka/src/main/java/com/qy105/aaa/ApplicationOne.java
@@ -0,0 +1,19 @@
+package com.qy105.aaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+/**
+ * @Description :
+ * @Author      : cat
+ * @exception   :
+ * @CreateDate  : 2019/12/2 10:25
+ * @Version     : 1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ApplicationOne {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationOne.class);
+    }
+}
--- cloud-1202-eurekaTwo/src/main/java/com/qy105/aaa/ApplicationTwo.java
+++ cloud-1202-eurekaTwo/src/main/java/com/qy105/aaa/ApplicationTwo.java
@@ -3,6 +3,7 @@ package com.qy105.aaa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * @Description :
  * @Author      : cat
@@ -12,8 +13,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class Application {
+public class ApplicationTwo {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        SpringApplication.run(ApplicationTwo.class);
     }
 }
